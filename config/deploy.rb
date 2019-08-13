5bc43fc2d43e610cc85e1a0a8034268c394e7a4a
Takashi <Takashi@gmail.com>
2019-08-13 15:48:13 +0900

    デプロイ前の確認２回目

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'aws-task'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/TakashiTakeyama/aws-task'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/aws-task'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
