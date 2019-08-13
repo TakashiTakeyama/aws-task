a93f2c2fa1385b4fcb12f776126dedde9b2ee598
Takashi <Takashi@gmail.com>
2019-08-13 16:18:18 +0900

    デプロイ前の確認3回目

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,6 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+server '54.248.24.38', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/takeyamatakashi/.ssh/id_rsa'
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,2 @@
-server '54.248.24.38', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/takeyamatakashi/.ssh/id_rsa'
+# server '54.248.24.38', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/takeyamatakashi/.ssh/id_rsa'
