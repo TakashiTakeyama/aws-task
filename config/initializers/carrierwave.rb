53d04aff23f3c2261d0e7f5c8e99a25ce8b22eb7
Takashi <Takashi@gmail.com>
2019-08-13 19:16:22 +0900

    調整

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id:     ENV["AWSAccessKeyId"],
+      aws_secret_access_key: ENV["AWSSecretKey"],
       region:                'ap-northeast-1',
       path_style:            true,
   }
@@ -18,8 +18,8 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'aws-kadai-production名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-kadai-production名'
+      config.fog_directory = 'aws-task-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-kadai-production'
     when 'development'
       config.fog_directory = 'aws-task-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/aws-task-production'
